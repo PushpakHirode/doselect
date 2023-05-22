a5b0b1590bbb989c2fc820cd890f9986ef383c58
Hirode <pushpak-purushottam.hirode@capgemini.com>
2023-05-22 11:46:41 +0530

    Java doselect folder added

---
+++ doselect/src/com/stringhandling/Source.java
@@ -0,0 +1,21 @@
+package com.stringhandling;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Source {
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(10,25,33,28,10,12));
+		
+		ArrayListOps obj = new ArrayListOps();
+		
+		ArrayList<Integer> list1 = obj.makeArrayListInt(4);
+		ArrayList<Integer> list2 = obj.reverseList(list);
+		ArrayList<Integer> list3 = obj.changeList(list2 , 28, 20);
+		
+		System.out.println(list1);
+		System.out.println(list2);
+		System.out.println(list3);
+	}
+}
